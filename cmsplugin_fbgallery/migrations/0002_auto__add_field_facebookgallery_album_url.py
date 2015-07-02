b894c14b02b3f5b7e8a4d61fdd0f1459ae5bea34
Justinas Jaronis <justinas.jaronis@aksprendimai.lt>
2015-07-02 12:18:12 +0300

    very dirty parser from facebook album url

---
+++ cmsplugin_fbgallery/migrations/0002_auto__add_field_facebookgallery_album_url.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'FacebookGallery.album_url'
+        db.add_column(u'cmsplugin_facebookgallery', 'album_url',
+                      self.gf('django.db.models.fields.CharField')(default='', max_length=1000),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'FacebookGallery.album_url'
+        db.delete_column(u'cmsplugin_facebookgallery', 'album_url')
+
+
+    models = {
+        'cms.cmsplugin': {
+            'Meta': {'object_name': 'CMSPlugin'},
+            'changed_date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'creation_date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'language': ('django.db.models.fields.CharField', [], {'max_length': '15', 'db_index': 'True'}),
+            'level': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'lft': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.CMSPlugin']", 'null': 'True', 'blank': 'True'}),
+            'placeholder': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.Placeholder']", 'null': 'True'}),
+            'plugin_type': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_index': 'True'}),
+            'position': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'rght': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'tree_id': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'})
+        },
+        'cms.placeholder': {
+            'Meta': {'object_name': 'Placeholder'},
+            'default_width': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'slot': ('django.db.models.fields.CharField', [], {'max_length': '255', 'db_index': 'True'})
+        },
+        u'cmsplugin_fbgallery.facebookgallery': {
+            'Meta': {'object_name': 'FacebookGallery', '_ormbases': ['cms.CMSPlugin']},
+            'album_id': ('django.db.models.fields.CharField', [], {'max_length': '500'}),
+            'album_name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'album_url': ('django.db.models.fields.CharField', [], {'max_length': '1000'}),
+            u'cmsplugin_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['cms.CMSPlugin']", 'unique': 'True', 'primary_key': 'True'})
+        }
+    }
+
+    complete_apps = ['cmsplugin_fbgallery']
--- cmsplugin_fbgallery/models.py
+++ cmsplugin_fbgallery/models.py
@@ -2,8 +2,18 @@ from django.db import models
 from cms.models.pluginmodel import CMSPlugin
 from django.utils.translation import ugettext_lazy as _
 
+import re
 
 class FacebookGallery(CMSPlugin):
-    album_id = models.CharField(_('Album Id'), max_length=500)
+    album_url = models.CharField(_('Album URL'), max_length=1000)
+    album_id = models.CharField(_('Album Id'), max_length=500, default='(detect automatically)')
     album_name = models.CharField(_('Album Name'), max_length=100)
+    
+    def save(self, *args, **kwargs):
+        p = re.compile('\d+')
+        ids = p.findall(self.album_url)
+        if len(ids) > 3:
+            self.album_id = '%s_%s' % (ids[2], ids[1])
+        res = super(FacebookGallery, self).save(*args, **kwargs)
+        return res
 
